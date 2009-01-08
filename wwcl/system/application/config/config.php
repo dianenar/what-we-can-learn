7b97344a27877924edb04019a5d38b9931ed0705
diane nardozzi <dianenar@gmail.com>
2009-01-08 09:07:05 -0500

    changed config to use index.php for local dev

--- wwcl/system/application/config/config.php
+++ wwcl/system/application/config/config.php
@@ -23,7 +23,7 @@ $config['base_url']	= "http://localhost/what-we-can-learn/wwcl/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "";
+$config['index_page'] = "index.php";
 
 /*
 |--------------------------------------------------------------------------
